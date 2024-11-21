ff8a935e7685a58c9cb90152342992de6e6dca25
Yelnaz Rysbek <yelnaz.rysbek@gmail.com>
2024-11-21 16:49:21 -0500

    ...

--- js/video.js
+++ js/video.js
@@ -1,11 +1,48 @@
-var video;
+var video = document.querySelector(".video");
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	video.autoplay = false;
+    video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+    video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+    video.playbackRate *= 0.9;
+    console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+    video.playbackRate *= 1.1;
+    console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+    video.currentTime = video.currentTime + 10 > video.duration ? 0 : video.currentTime + 10;
+    console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    video.muted = !video.muted;
+    document.querySelector("#mute").innerHTML = video.muted ? "Unmute" : "Mute";
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+    video.volume = document.querySelector("#slider").value / 100;
+    document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+    video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+});
 
